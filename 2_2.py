2bd02b6fce837c4a6168a90241713ad307f59be7
Unique Orduna <uniqueorduna@swuser-129-8-207-109.swlan.csufresno.edu>
2022-02-22 15:35:00 -0800

    class lecture notes

---
+++ 2_2.py
@@ -0,0 +1,42 @@
+
+myvar = "Say it aint so" 
+print (myvar)
+
+var2 = "Spam & eggs"
+print ("var2")
+print (var2[0])
+
+var2[2:7]
+print (var2[2:7])
+print (var2[:])
+
+
+# var2 = "Spam & Eggs"
+# spam = Spam
+# eggs = Eggs
+# spam_and_eggs = spam + "&" + eggs
+# print (spam_and_eggs[:])
+
+# praise = "Good Doggie"
+
+# count = praise.upper().count("G")
+
+# print("Count: ", count)
+
+# town = input ("Enter the name of your city:")
+
+fullName = input ("Enter a full name: ")
+n = fullName.rfind (" ")
+print ("Last name:", fullName [n+1:])
+print("First name (s):", fullName[:n])
+
+
+# print(int("23"))
+# print(float("23"))
+# print(eval("23"))
+# print(eval("23.5"))
+# x = 5
+# print(eval("23 + (2 * x)"))
+
+
+
