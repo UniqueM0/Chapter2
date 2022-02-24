5b9f704eba0f79671502a3fc7a4ad4908d729a93
Unique Orduna <uniqueorduna@swuser-129-8-204-141.swlan.csufresno.edu>
2022-02-24 15:42:18 -0800

    notes

--- 2_3.py
+++ 2_3.py
@@ -13,3 +13,8 @@ from os import sep
 # print("a\tb\tc".expandtabs(5))  #a     b     c
 # print("Nudge, \tnudge, \nwink, \twing.".expandtabs(11))  #11Spaces inbetween     
  
+print("012345678901234567")
+print("rank".ljust(5), "player".ljust(20), "HR".rjust(3), sep="")
+print('1'.center(5), "barry_bonds".ljust(20),"762".rjust(3),sep="")
+print('2'.center(5),"hank_aaron".ljust(20), "755".rjust(3), sep="")
+print('3'.center(5), "babe_ruth".ljust(20), "714".rjust(3), sep="")
