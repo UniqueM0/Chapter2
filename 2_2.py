5a0c10859a484584baede625624beaebee9c7223
Unique Orduna <uniqueorduna@swuser-129-8-204-141.swlan.csufresno.edu>
2022-02-24 14:48:36 -0800

    2.2

--- 2_2.py
+++ 2_2.py
@@ -1,14 +1,4 @@
 
-myvar = "Say it aint so" 
-print (myvar)
-
-var2 = "Spam & eggs"
-print ("var2")
-print (var2[0])
-
-var2[2:7]
-print (var2[2:7])
-print (var2[:])
 
 
 # var2 = "Spam & Eggs"
@@ -25,10 +15,10 @@ print (var2[:])
 
 # town = input ("Enter the name of your city:")
 
-fullName = input ("Enter a full name: ")
-n = fullName.rfind (" ")
-print ("Last name:", fullName [n+1:])
-print("First name (s):", fullName[:n])
+# full_Name = input ("Enter a full name: ")
+# index_of_Space = full_Name.rfind (" ")
+# print ("Last name:", full_Name [index_of_Space+1:])
+# print("First name (s):", full_Name[:index_of_Space])
 
 
 # print(int("23"))
@@ -37,6 +27,31 @@ print("First name (s):", fullName[:n])
 # print(eval("23.5"))
 # x = 5
 # print(eval("23 + (2 * x)"))
+
+# full_Name = input ("Enter your full name: ")
+# index_of_space = full_Name.rfind(" ")
+# first_name = full_Name[:index_of_space]
+# last_name = full_Name[index_of_space+1:]
+# print("First_name", first_name)
+# print("Last_name", last_name)
+
+# annual_ income = input("Enter your annual income: ")
+# print("User's income is", income)
+# monthly_income = annual_income/12
+# print ("monthlt_income", monthly_income)
+
+# annual_income = eval(input("Enter your annual income : "))
+# print ("User's income is ", annual_income)
+# monthly_income = annual_income/12
+# print ("monthlty income", monthly_income)
+
+str1 = "Python"
+length = len(str1) # 5Int
+str2 = str1[:] #Python String
+print("str2", str2)
+# str3 = str1[10] #Error Code There is not 10 index
+str4 = str2[2:-1] 
+print("str4", str4)
 
 
 
