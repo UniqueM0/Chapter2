75ae3194a45377a4ad234768306b20e589b14ee0
Unique Orduna <uniqueorduna@swuser-129-8-204-141.swlan.csufresno.edu>
2022-02-24 15:28:04 -0800

    New info

--- 2_3.py
+++ 2_3.py
@@ -1,1 +1,15 @@
-print("chapter 2_3")
+# print("chapter 2_3")
+
+from os import sep
+
+
+# print("one","two","three", sep=",")
+
+# print("Hello", end=" ")
+# print("World!")
+
+# print("01234567890123456")
+# print("a\tb\tc") #a     b      c
+# print("a\tb\tc".expandtabs(5))  #a     b     c
+# print("Nudge, \tnudge, \nwink, \twing.".expandtabs(11))  #11Spaces inbetween     
+ 
