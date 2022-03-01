a955f2597e99bc0361c202e5223589bf994725dc
Unique Orduna <uniqueorduna@swuser-129-8-205-73.swlan.csufresno.edu>
2022-03-01 14:54:24 -0800

    new notes

--- 2_4.py
+++ 2_4.py
@@ -1,1 +1,13 @@
-print("Hello_There")
+# print("Hello_There")
+
+# print("{0:10d}".format(12345678))
+# print("{0:10,d}".format(12345678))
+# print("{0:10.2f}".format(1234.5678))
+# print("{0:10,.2f}".format(1234.5678))
+# print("{0:10,.3f}".format(1234.5678))
+# print("{0:10.2%}".format(12.345678))
+# print("{0:10,.3%}".format(12.34569))
+
+print("The area of {0:s} is {1:,d} square miles.".format("Texas",268820))
+str1 = "The population of {0:s} is {1:.2%} of the U.S. Population."
+print(str1.format("Texas", 2))
