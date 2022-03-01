6fd8cb44477eb6fe5d28787c33b30a2af9051f8b
Unique Orduna <uniqueorduna@swuser-129-8-205-73.swlan.csufresno.edu>
2022-03-01 15:32:55 -0800

    notes

--- 2_4.py
+++ 2_4.py
@@ -8,6 +8,19 @@
 # print("{0:10.2%}".format(12.345678))
 # print("{0:10,.3%}".format(12.34569))
 
-print("The area of {0:s} is {1:,d} square miles.".format("Texas",268820))
-str1 = "The population of {0:s} is {1:.2%} of the U.S. Population."
-print(str1.format("Texas", 2))
+# print("The area of {0:s} is {1:,d} square miles.".format("Texas",268820))
+# str1 = "The population of {0:s} is {1:.2%} of the U.S. Population."
+# print(str1.format("Texas", 2)) 
+
+# num1 = 1
+# num2 = 10
+# num3 = 1000
+# sum_of_all_numbers = num1 + num2 + num3
+# # print ("Sum is {}".format(sum_of_all_numbers))
+# print("Sum is {0:n}".format(sum_of_all_numbers))
+
+# list_of_numbers = [1, 10,1000]
+# print(sum(list_of_numbers))
+
+list_of_numbers =[1,23,56,78]
+print(list_of_numbers.count(23))
