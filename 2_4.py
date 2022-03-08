caeb6526ab011da0b460c81b1e9ba93763364607
Unique Orduna <uniqueorduna@swuser-129-8-206-32.swlan.csufresno.edu>
2022-03-08 15:09:19 -0800

    added average

--- 2_4.py
+++ 2_4.py
@@ -19,9 +19,59 @@
 # # print ("Sum is {}".format(sum_of_all_numbers))
 # print("Sum is {0:n}".format(sum_of_all_numbers))
 
-# list_of_numbers = [1, 10,1000]
-# print(sum(list_of_numbers))
+# # list_of_numbers = [1, 10,1000]
+# # print(sum(list_of_numbers))
 
-list_of_numbers =[1,23,56,78]
-print(list_of_numbers.count(23))
+# # list_of_numbers =[1,23,56,78]
+# # print(list_of_numbers.count(23))
 
+# # list_of_numbers = [1, 10, 1000]
+# # print(list_of_numbers)
+# # list_of_numbers.clear()
+# # print(list_of_numbers)
+# # list_of_numbers.append(100)
+# # print(list_of_numbers)
+
+# list_of_numbers = [1, 10, 1000]
+# print(list_of_numbers)
+# list_of_numbers.clear()
+# print(list_of_numbers)
+# list_of_numbers.append(100)
+# list_of_numbers.append(100)
+# list_of_numbers.append(100)
+# list_of_numbers.append(100)
+# print(list_of_numbers)
+
+# list_of_numbers2 = [3, 100, 5]
+# list_of_numbers.extend(list_of_numbers2)
+# print(list_of_numbers)
+
+# l1 = [1, 32, 4]
+# l2 = [100, 200, 300]
+# l3 = l1 +l2 
+# print(l3)
+
+# words = ["Spam", "wink", "Hi"]
+# words.insert(1, "Hello")
+
+# print("Words======>", words)
+
+## Calculate average of grades
+grades = [] # create the variable grades and assign it the empty list 
+num = float(input("Enter the first grade: "))
+grades.append(num)
+num = float(input("Enter the second grade: "))
+grades.append(num)
+num = float(input("Enter the third grade: "))
+grades.append(num)
+num = float(input("Enter the fourth grade: "))
+grades.append(num)
+num = float(input("Enter the fifth grade: "))
+grades.append(num)
+
+minimum_grade = min(grades)
+grades.remove(minimum_grade)
+minimum_grade = min(grades)
+grades.remove(minimum_grade)
+average = sum(grades) / len(grades)
+print("Average Grade: {0: .2f}".format(average))
