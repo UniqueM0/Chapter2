d1001a60f41faff7f93fa94d96c9370ec3c423bc
Unique Orduna <uniqueorduna@swuser-129-8-207-20.swlan.csufresno.edu>
2022-03-10 15:24:38 -0800

    class notes

--- 2_4.py
+++ 2_4.py
@@ -38,40 +38,105 @@
 # print(list_of_numbers)
 # list_of_numbers.append(100)
 # list_of_numbers.append(100)
-# list_of_numbers.append(100)
-# list_of_numbers.append(100)
-# print(list_of_numbers)
+# # list_of_numbers.append(100)
+# # list_of_numbers.append(100)
+# # print(list_of_numbers)
 
-# list_of_numbers2 = [3, 100, 5]
-# list_of_numbers.extend(list_of_numbers2)
-# print(list_of_numbers)
+# # list_of_numbers2 = [3, 100, 5]
+# # list_of_numbers.extend(list_of_numbers2)
+# # print(list_of_numbers)
 
-# l1 = [1, 32, 4]
-# l2 = [100, 200, 300]
-# l3 = l1 +l2 
-# print(l3)
+# # l1 = [1, 32, 4]
+# # l2 = [100, 200, 300]
+# # l3 = l1 +l2 
+# # print(l3)
 
-# words = ["Spam", "wink", "Hi"]
-# words.insert(1, "Hello")
+# # words = ["Spam", "wink", "Hi"]
+# # words.insert(1, "Hello")
 
-# print("Words======>", words)
+# # print("Words======>", words)
 
-## Calculate average of grades
-grades = [] # create the variable grades and assign it the empty list 
-num = float(input("Enter the first grade: "))
-grades.append(num)
-num = float(input("Enter the second grade: "))
-grades.append(num)
-num = float(input("Enter the third grade: "))
-grades.append(num)
-num = float(input("Enter the fourth grade: "))
-grades.append(num)
-num = float(input("Enter the fifth grade: "))
-grades.append(num)
+# ## Calculate average of grades
+# grades = [] # create the variable grades and assign it the empty list 
+# num = float(input("Enter the first grade: "))
+# grades.append(num)
+# # num = float(input("Enter the second grade: "))
+# # grades.append(num)
+# # num = float(input("Enter the third grade: "))
+# # grades.append(num)
+# # num = float(input("Enter the fourth grade: "))
+# # grades.append(num)
+# # num = float(input("Enter the fifth grade: "))
+# # grades.append(num)
 
-minimum_grade = min(grades)
-grades.remove(minimum_grade)
-minimum_grade = min(grades)
-grades.remove(minimum_grade)
-average = sum(grades) / len(grades)
-print("Average Grade: {0: .2f}".format(average))
+# # minimum_grade = min(grades)
+# # grades.remove(minimum_grade)
+# # minimum_grade = min(grades)
+# # grades.remove(minimum_grade)
+# # average = sum(grades) / len(grades)
+# # print("Average Grade: {0: .2f}".format(average))
+
+# # grades = []
+# # grades.append(1)
+# # grades.append(2)
+# # grades.append(4)
+# # grades.append(9)
+
+# # print("Grades", grades)
+
+# # length = len(grades)
+# # print("length", length)
+# # print("sliced:", grades[0:2])
+
+# print("a,b,c".split(','))
+# print("a**b**c".split('**'))
+# print("a\nb\nc".split())
+# print("a b c".split())
+
+# str1 = "a,b,c"
+# parts = str1.split(",")
+# # # parts = "a,b,c".split(",")   #can do either one
+# # print("parts: ", parts)
+
+# # lines = ["To", "be", "or", "not", "to", "be"]
+
+# # print("Line before join: ", lines)
+
+# # joined = " ".join(lines)   # Depending on what is put in "" that will be the sperator 
+
+# # print("joined: ", joined)
+
+# #open up the Data.txt file in read mode
+# # infile = open("Data.txt", 'r')
+
+# # print("infile", infile)
+# # names = []
+# # for line in infile: 
+# #     names.append(line.rstrip)
+# #     # print("line: ", line)
+# #     # print("line after striping the right side: ", line.rstrip())
+
+# # #close the Data.tx file to preserve memory
+# # infile = open
+# # names_using_list_comprehension = [line.rstrip() for line in infile]
+# # # print("names_using_list_comprehension: ", names_using_list_comprehension)
+# # infile.close()
+
+
+# infile = open("Grades.txt", "r")
+# for line in infile:
+#     print("line: ", line.rstrip())
+# infile.close()
+
+# infile = open("Grades.txt", "r")
+# list_of_numbers = [eval(line) for line in infile]
+# infile.close()
+
+
+# list_of_names = ("Lucas", "John", ["A", (1, 2, 3), "C"])
+# print("list_of_names", list_of_names[2][1][-1])
+
+list1 = ["A", "B", "C"]
+list2 = list(list1)
+list2.append("D")
+print(list1)
